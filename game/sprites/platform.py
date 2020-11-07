e9bc11732a5782cb2bc4cc85a9bf3872760355fc
TudorLEvans <55924569+TudorLEvans@users.noreply.github.com>
2020-11-07 15:31:55 +0000

    add pause menu

--- game/sprites/platform.py
+++
@@ -1,9 +0,0 @@
-import pygame,sys
-
-class Platform(pygame.sprite.Sprite):
-    def __init__(self, position_x,position_y):
-        super(Platform, self).__init__()
-        self.surf = pygame.Surface((50, 50))
-        self.surf.fill((200, 50, 50))
-        self.rect = self.surf.get_rect()
-        self.rect.topleft = position_x,position_y
---
+++ menu/pause.py
@@ -0,0 +1,28 @@
+import pygame
+import pygame_menu
+from game import main
+from pygame_menu import themes
+
+mytheme = themes.Theme(
+    title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_NONE,
+    widget_font = pygame_menu.font.FONT_8BIT,
+    title_font = pygame_menu.font.FONT_8BIT,
+    title_offset=(20,20),
+    background_color = (0,0,0),
+    title_font_size = 30,
+    menubar_close_button = False
+)
+
+def create_pause_menu(screen, screen_width, screen_height, exit_game):
+
+    pause_menu = pygame_menu.Menu(
+        screen_width, 
+        screen_height, 
+        'Fly me to the moon',
+        theme=mytheme
+    )
+
+    pause_menu.add_button('Return to Game', pause_menu.disable)
+    pause_menu.add_button('Main Menu', pause_menu.full_reset)
+    pause_menu.add_button('Quit', pygame_menu.events.EXIT)
+    return pause_menu
